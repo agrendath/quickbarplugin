86b0e4c2d07dd9309eb6823bdaeb883077cd9e0d
agrendath <mendo.leal@gmail.com>
2020-08-07 14:47:58 +0200

    Added bow as a valid enchantment type for Harvesting enchantment

--- src/main/java/com/gmail/leal/mendo/QuickbarPlugin/QuickbarPlugin.java
+++ src/main/java/com/gmail/leal/mendo/QuickbarPlugin/QuickbarPlugin.java
@@ -59,7 +59,7 @@ import com.gmail.nossr50.util.player.UserManager;
 public class QuickbarPlugin extends JavaPlugin implements Listener{
 	
 	private final static List<Material> validAbsorptionTypes = new ArrayList<Material>(Arrays.asList(new Material[] {Material.WOODEN_AXE, Material.STONE_AXE, Material.IRON_AXE, Material.GOLDEN_AXE, Material.DIAMOND_AXE, Material.NETHERITE_AXE, Material.WOODEN_PICKAXE, Material.STONE_PICKAXE, Material.IRON_PICKAXE, Material.GOLDEN_PICKAXE, Material.DIAMOND_PICKAXE, Material.NETHERITE_PICKAXE, Material.WOODEN_SHOVEL, Material.STONE_SHOVEL, Material.IRON_SHOVEL, Material.GOLDEN_SHOVEL, Material.DIAMOND_SHOVEL, Material.NETHERITE_SHOVEL, Material.WOODEN_HOE, Material.STONE_HOE, Material.IRON_HOE, Material.GOLDEN_HOE, Material.DIAMOND_HOE, Material.NETHERITE_HOE, Material.BOW, Material.WOODEN_SWORD, Material.STONE_SWORD, Material.IRON_SWORD, Material.GOLDEN_SWORD, Material.DIAMOND_SWORD, Material.NETHERITE_SWORD})); 
-	private final static List<Material> validDoublexpTypes = new ArrayList<Material>(Arrays.asList(new Material[] {Material.WOODEN_SWORD, Material.STONE_SWORD, Material.IRON_SWORD, Material.GOLDEN_SWORD, Material.DIAMOND_SWORD, Material.NETHERITE_SWORD}));
+	private final static List<Material> validDoublexpTypes = new ArrayList<Material>(Arrays.asList(new Material[] {Material.WOODEN_SWORD, Material.STONE_SWORD, Material.IRON_SWORD, Material.GOLDEN_SWORD, Material.DIAMOND_SWORD, Material.NETHERITE_SWORD, Material.BOW}));
 	final static String ENCHANTMENT_INDESTRUCTIBILITY = "Indestructibility";
 	final static String ENCHANTMENT_ABSORPTION = "Magnetism";
 	final static String ENCHANTMENT_DOUBLEXP = "Harvesting";
