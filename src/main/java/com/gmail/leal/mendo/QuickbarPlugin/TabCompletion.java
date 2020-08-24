e3b17d46f81130d968bf3682e5e1842c49311352
agrendath <mendo.leal@gmail.com>
2020-08-24 15:16:51 +0100

    Added autocomplete for skills with /booster <skillName>

--- src/main/java/com/gmail/leal/mendo/QuickbarPlugin/TabCompletion.java
+++ src/main/java/com/gmail/leal/mendo/QuickbarPlugin/TabCompletion.java
@@ -9,6 +9,8 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.util.StringUtil;
 
+import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
+
 public class TabCompletion implements TabCompleter{
 	@Override
     public List<String> onTabComplete (CommandSender sender, Command cmd, String label, String[] args)  {
@@ -16,6 +18,12 @@ public class TabCompletion implements TabCompleter{
 		if(cmd.getName().equalsIgnoreCase("soulenchant") && args.length == 1 && sender instanceof Player)  {
 			for(String enchantment : SoulEnchantments.getSoulEnchantments())  {
 				result.add(enchantment);
+			}
+			return StringUtil.copyPartialMatches(args[0], result, new ArrayList<String>());
+		}
+		else if(cmd.getName().equalsIgnoreCase("booster") && args.length == 1 && sender instanceof Player)  {
+			for(PrimarySkillType skill : PrimarySkillType.values())  {
+				result.add(skill.getName());
 			}
 			return StringUtil.copyPartialMatches(args[0], result, new ArrayList<String>());
 		}
